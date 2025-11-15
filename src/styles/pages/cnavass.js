24b0a917fbd33dc9d9f3dbf120903085b3f7ea51
rimabmousavi <rimamousavi.team@gmail.com>
2025-11-15 16:20:39 +0330

    fixed: download btn

--- src/styles/pages/cnavass.js
+++ src/styles/pages/cnavass.js
@@ -9,12 +9,14 @@ function openMenu() {
     menu.classList.add('open');
     hamburgerContainer.classList.add('is-active');
     overlay.classList.remove('hidden');
+    document.body.classList.add('no-scroll');
 }
 
 function closeMenu() {
     menu.classList.remove('open');
     hamburgerContainer.classList.remove('is-active');
     overlay.classList.add('hidden');
+    document.body.classList.remove('no-scroll');
 }
 
 openBtn.addEventListener('click', openMenu);
